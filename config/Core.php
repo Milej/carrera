d0e8978557475bd35eca7fdbc7eb4337e451344c
Max Menvielle <maxmenvielle98@gmail.com>
2022-11-17 01:05:12 -0300

    Reparacion archivo COnfig para poder subir

--- config/Core.php
+++ config/Core.php
@@ -5,10 +5,10 @@ define('APP_NAME', 'Desafio Los Reartes');
 
 // Carpeta de la aplicacion
 /* En caso de que la aplicación esté en la carpeta raíz del servidor dejar vacio */ 
-define('APP_FOLDER', '/carrera/');
+define('APP_FOLDER', '/');
 
 // Estado de aplicacion
-define('ENVIROMENT', 'dev');
+define('ENVIROMENT', 'production');
 
 // Timezone aplicacion
 define('TIMEZONE', 'America/Cordoba');
