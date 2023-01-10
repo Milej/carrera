d8a44f793004a0c2608044007b3cb9238ddaa137
Max Menvielle <maxmenvielle98@gmail.com>
2023-01-10 15:25:34 -0300

    Se arreglo la vista para mobile

--- resources/modules/sponsors.php
+++ resources/modules/sponsors.php
@@ -3,71 +3,71 @@
 
         <h1>NOS ACOMPAÑAN</h1>
 
-        <div class="col-lg-2 col-3 text-center img-sponsor-container">
+        <div class="col-lg-2 col-6 text-center img-sponsor-container">
             <img src="./public/images/sponsors/logo-sram.png" alt="" class="img-sponsor">
         </div>
 
-        <div class="col-lg-2 col-3 text-center img-sponsor-container">
+        <div class="col-lg-2 col-6 text-center img-sponsor-container">
             <img src="./public/images/sponsors/logo-cc9.png" alt="" class="img-sponsor">
         </div>
 
-        <div class="col-lg-2 col-3 text-center img-sponsor-container">
+        <div class="col-lg-2 col-6 text-center img-sponsor-container">
             <img src="./public/images/sponsors/logo-nutremax.png" alt="" class="img-sponsor">
         </div>
 
-        <div class="col-lg-2 col-3 text-center img-sponsor-container">
+        <div class="col-lg-2 col-6 text-center img-sponsor-container">
             <img src="./public/images/sponsors/logo-optica.png" alt="" class="img-sponsor">
         </div>
 
-        <div class="col-lg-2 col-3 text-center img-sponsor-container">
+        <div class="col-lg-2 col-6 text-center img-sponsor-container">
             <img src="./public/images/sponsors/logo-sb.png" alt="" class="img-sponsor">
         </div>
 
 
-        <div class="col-lg-2 col-3 text-center img-sponsor-container">
+        <div class="col-lg-2 col-6 text-center img-sponsor-container">
             <img src="./public/images/sponsors/logo-hidalgo.png" alt="" class="img-sponsor">
         </div>
         
-        <div class="col-lg-2 col-3 text-center img-sponsor-container">
+        <div class="col-lg-2 col-6 text-center img-sponsor-container">
             <img src="./public/images/sponsors/logo-sis.png" alt="" class="img-sponsor">
         </div>
 
-        <div class="col-lg-2 col-3 text-center img-sponsor-container">
+        <div class="col-lg-2 col-6 text-center img-sponsor-container">
             <img src="./public/images/sponsors/logo-microon.jpeg" alt="" class="img-sponsor">
         </div>
         
         
         
         <div class="col-12 row">
-            <div class="col-lg-1 col-sm-2 col-3 text-center img-sponsor-container">
+            <div class="col-lg-1 col-sm-2 col-4 text-center img-sponsor-container">
                 <img src="./public/images/sponsors/logo-lito.png" alt="" class="img-sponsor">
             </div>
-            <div class="col-lg-1 col-sm-2 col-3 text-center img-sponsor-container">
+            <div class="col-lg-1 col-sm-2 col-4 text-center img-sponsor-container">
                 <img src="./public/images/sponsors/logo-jc.png" alt="" class="img-sponsor">
             </div>
-            <div class="col-lg-1 col-sm-2 col-3 text-center img-sponsor-container">
+            <div class="col-lg-1 col-sm-2 col-4 text-center img-sponsor-container">
                 <img src="./public/images/sponsors/logo-foton.jpeg" alt="" class="img-sponsor">
             </div>
-            <div class="col-lg-1 col-sm-2 col-3 text-center img-sponsor-container">
+            <div class="col-lg-1 col-sm-2 col-4 text-center img-sponsor-container">
                 <img src="./public/images/sponsors/logo-hs.png" alt="" class="img-sponsor">
             </div>
-            <div class="col-lg-1 col-sm-2 col-3 text-center img-sponsor-container">
+            <div class="col-lg-1 col-sm-2 col-4 text-center img-sponsor-container">
                 <img src="./public/images/sponsors/logo-tacuifi.jpeg" alt="" class="img-sponsor">
             </div>
-            <div class="col-lg-1 col-sm-2 col-3 text-center img-sponsor-container">
+            <div class="col-lg-1 col-sm-2 col-4 text-center img-sponsor-container">
                 <img src="./public/images/sponsors/logo-ramona.png" alt="" class="img-sponsor">
             </div>
-            <div class="col-lg-1 col-sm-2 col-3 text-center img-sponsor-container">
+            <div class="col-lg-1 col-sm-2 col-4 text-center img-sponsor-container">
                 <img src="./public/images/sponsors/logo-ulpiano.png" alt="" class="img-sponsor">
             </div>
-            <div class="col-lg-1 col-sm-2 col-3 text-center img-sponsor-container">
+            <div class="col-lg-1 col-sm-2 col-4 text-center img-sponsor-container">
                 <img src="./public/images/sponsors/logo-zeven.jpeg" alt="" class="img-sponsor">
             </div>
-            <div class="col-lg-1 col-sm-2 col-3 text-center img-sponsor-container">
+            <div class="col-lg-1 col-sm-2 col-4 text-center img-sponsor-container">
                 <img src="./public/images/sponsors/logo-fotografia.png" alt="" class="img-sponsor">
             </div>
 
-            <div class="col-lg-1 col-sm-2 col-3 text-center img-sponsor-container">
+            <div class="col-lg-1 col-sm-2 col-4 text-center img-sponsor-container">
                 <img src="./public/images/sponsors/logo-panificacion.png" alt="" class="img-sponsor">
             </div>
         </div>
