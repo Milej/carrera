bb70ecfefd1b6532ba13a2df0f7b19791e5d0951
Max Menvielle <maxmenvielle98@gmail.com>
2023-01-12 16:29:38 -0300

    Se cambio el enlace a mytime

--- resources/modules/intro.php
+++ resources/modules/intro.php
@@ -2,5 +2,5 @@
     <h1>DESAFÍO LOS REARTES, CARRERA POR ETAPAS</h1>
     <h3>LOS REARTES - CÓRDOBA - ARGENTINA</h3>
     <h4>18, 19, 20 y 21 DE FEBRERO</h4>
-    <a class="btn btnInscribirse" aria-current="page" href="https://speedbike.com.ar/Inscripcion/DesafioLosReartesInfo" target="_blank" id="btnInscribirse">QUIERO PARTICIPAR</a>
+    <a class="btn btnInscribirse" aria-current="page" href="https://mytime.com.ar/Inscripcion/DesafioLosReartesInfo" target="_blank" id="btnInscribirse">QUIERO PARTICIPAR</a>
 </div>
